47657e8b4ec00b2d3064e9b8de7ac9f96e0a710b
Adam Miller <github@adammill.dev>
2025-12-24 16:49:50 -0800

    setup initial neovim plugins and lsp for nix

--- home-manager/programs/neovim.nix
+++ home-manager/programs/neovim.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }: {
   config = {
@@ -8,6 +9,35 @@
       enable = true;
       viAlias = true;
       vimAlias = true;
+      plugins = {
+        lspconfig.enable = true;
+        lsp-format.enable = true;
+        blink-cmp = {
+          enable = true;
+          settings.keymap.preset = "super-tab";
+        };
+        telescope.enable = true;
+        neo-tree.enable = true;
+        web-devicons.enable = true;
+      };
+      lsp = {
+        inlayHints.enable = true;
+
+        servers = {
+          nil_ls = {
+            enable = true;
+            config.settings.nil = {
+              formatting.command = ["${lib.getExe pkgs.alejandra}"];
+            };
+          };
+          nixd = {
+            enable = true;
+            config.settings = {
+              formatting.command = ["${lib.getExe pkgs.alejandra}"];
+            };
+          };
+        };
+      };
     };
   };
 }
