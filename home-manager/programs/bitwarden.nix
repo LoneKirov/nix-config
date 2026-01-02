38824fd2edf46d735b017fab1077de42ac3305d8
Adam Miller <github@adammill.dev>
2026-01-01 20:28:59 -0800

    enable rbw for bitwarden cli access

--- home-manager/programs/bitwarden.nix
+++ home-manager/programs/bitwarden.nix
@@ -9,6 +9,22 @@
   config = lib.mkMerge [
     {programs.bw.enable = lib.mkDefault true;}
     (lib.mkIf config.programs.bw.enable {
+      programs.rbw = {
+        enable = true;
+        # workaround for email being required in HM settings
+        package = pkgs.symlinkJoin {
+          name = "rbw-wrapped";
+          paths = let
+            pinentry =
+              if config.services.xserver.enable
+              then lib.getExe' pkgs.pinentry-gnome3 "pinentry-gnome3"
+              else lib.getExe' pkgs.pinentry-curses "pinentry-curses";
+          in [
+            (pkgs.writeShellScriptBin "pinentry" ''${pinentry} "$@"'')
+            pkgs.rbw
+          ];
+        };
+      };
       home.packages = with pkgs; [
         bitwarden-cli
       ];
