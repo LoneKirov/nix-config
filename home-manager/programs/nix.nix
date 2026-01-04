d20609b4b1c028eab65d6f0a36b90e2f3fa3674d
Adam Miller <github@adammill.dev>
2026-01-04 01:14:54 -0800

    alias nixos-rebuild so the correct flags are passed by default

--- home-manager/programs/nix.nix
+++ home-manager/programs/nix.nix
@@ -5,9 +5,14 @@
   ...
 }: {
   config = lib.mkIf config.nix.enable {
-    home.packages = with pkgs; [
-      cachix
-    ];
+    home = {
+      packages = with pkgs; [
+        cachix
+      ];
+      shellAliases = {
+        nixos-rebuild = "nixos-rebuild --sudo --flake $HOME/nix-config";
+      };
+    };
 
     programs.zsh.zplug.plugins = [
       {name = "nix-community/nix-zsh-completions";}
