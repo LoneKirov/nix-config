8b4c9d42dccb2754fff926f4feb1d06dee5afdbb
Adam Miller <github@adammill.dev>
2026-01-14 18:06:10 -0800

    update system
    - update flake.lock
    - switch to kernel 6.19-rc4 to unblock updating

--- hosts/mew/nixos/framework-amd-ai-300-series/hibernate/default.nix
+++ hosts/mew/nixos/framework-amd-ai-300-series/hibernate/default.nix
@@ -15,18 +15,7 @@
   # use suspend-then-hibernate when closing the lid
   services.logind.settings.Login.HandleLidSwitch = "suspend-then-hibernate";
 
-  boot = {
-    # Hibernate is broken on kernels >6.17.9
-    # https://bugzilla.kernel.org/show_bug.cgi?id=220858
-    kernelPackages = lib.mkForce (pkgs.linuxPackagesFor (pkgs.linux_6_17.override {
-      argsOverride = rec {
-        src = pkgs.fetchurl {
-          url = "mirror://kernel/linux/kernel/v6.x/linux-${version}.tar.xz";
-          sha256 = "sha256-bQiAO5U8UJ30jUTTKB7TklJDIdi7NT6yHAVVeQyPjgY=";
-        };
-        version = "6.17.9";
-        modDirVersion = "6.17.9";
-      };
-    }));
-  };
+  # Hibernate is broken on kernels >6.17.9 and <6.19
+  # https://bugzilla.kernel.org/show_bug.cgi?id=220858
+  boot.kernelPackages = lib.mkForce pkgs.linuxPackages_testing;
 }
