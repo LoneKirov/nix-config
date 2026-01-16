a60363652ca107acb1a8cd58083a38bc1cf382f2
Adam Miller <github@adammill.dev>
2026-01-15 22:46:42 -0800

    nixvim updates
    - enable neogit plugin
    - add telescope keybinds
    - enable telescope fzf-native
    - remove Neotree git_status keybind
    - enable yazi plugin
    - enable diffview plugin

--- modules/nixvim/default.nix
+++ modules/nixvim/default.nix
@@ -1,4 +1,12 @@
-{...}: {
+{
+  hmConfig,
+  lib,
+  ...
+}: let
+  yazi = hmConfig.programs.yazi.enable;
+  git = hmConfig.programs.git.enable;
+  fzf = hmConfig.programs.fzf.enable;
+in {
   imports = [./lsp];
 
   config = {
@@ -11,24 +19,93 @@
         enable = true;
         settings.keymap.preset = "super-tab";
       };
-      telescope.enable = true;
-      neo-tree.enable = true;
+      telescope = {
+        enable = true;
+        extensions.fzf-native.enable = fzf;
+      };
+      neo-tree = {
+        enable = true;
+        settings = {
+          close_if_last_window = true;
+          filesystem = {
+            follow_current_file = {
+              enabled = true;
+              leave_dirs_open = true;
+            };
+          };
+        };
+      };
+      diffview.enable = true;
+      neogit.enable = git;
       web-devicons.enable = true;
+      yazi.enable = yazi;
     };
     lsp = {
       inlayHints.enable = true;
     };
-    keymaps = [
-      {
-        action = "<cmd>Neotree filesystem<CR>";
-        key = "<leader>ntf";
-        mode = "n";
-      }
-      {
-        action = "<cmd>Neotree git_status<CR>";
-        key = "<leader>ntg";
-        mode = "n";
-      }
-    ];
+    keymaps =
+      [
+        {
+          action = "<cmd>Neotree filesystem<CR>";
+          key = "<leader>ntf";
+          mode = "n";
+        }
+
+        {
+          action = "<cmd>Telescope find_files<CR>";
+          key = "<leader>tff";
+          mode = "n";
+        }
+
+        {
+          action = "<cmd>Telescope grep_string<CR>";
+          key = "<leader>tg";
+          mode = "n";
+        }
+        {
+          action = "<cmd>Telescope live_grep<CR>";
+          key = "<leader>tlg";
+          mode = "n";
+        }
+        {
+          action = "<cmd>Telescope buffers<CR>";
+          key = "<leader>tb";
+          mode = "n";
+        }
+        {
+          action = "<cmd>Telescope current_buffer_fuzzy_find<CR>";
+          key = "<leader>tbff";
+          mode = "n";
+        }
+      ]
+      ++ lib.optionals git [
+        {
+          action = "<cmd>Telescope git_files<CR>";
+          key = "<leader>tgf";
+          mode = "n";
+        }
+        {
+          action = "<cmd>Telescope git_commits<CR>";
+          key = "<leader>tgc";
+          mode = "n";
+        }
+        {
+          action = "<cmd>Telescope git_status<CR>";
+          key = "<leader>tgs";
+          mode = "n";
+        }
+        {
+          action = "<cmd>Neogit<CR>";
+          key = "<leader>gg";
+          mode = "n";
+        }
+      ]
+      ++ lib.optionals yazi [
+        {
+          action = "<cmd>Yazi<CR>";
+          key = "<leader>yy";
+          mode = "n";
+        }
+      ];
   };
 }
