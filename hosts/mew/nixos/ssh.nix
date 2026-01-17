c66b78a0fd1897eeeda20740d5a471a0a0f32491
Adam Miller <github@adammill.dev>
2026-01-16 21:45:43 -0800

    create afuse service to automount sshfs mounts

--- hosts/mew/nixos/ssh.nix
+++ hosts/mew/nixos/ssh.nix
@@ -1,16 +1,38 @@
-{...}: {
-  home-manager.users.kirov.programs.ssh = {
-    enable = true;
-    enableDefaultConfig = false;
-    matchBlocks = {
-      moltres = {
-        host = "moltres";
-        forwardAgent = true;
+{pkgs, ...}: {
+  home-manager.users.kirov = {
+    programs.ssh = {
+      enable = true;
+      enableDefaultConfig = false;
+      matchBlocks = {
+        moltres = {
+          host = "moltres";
+          forwardAgent = true;
+        };
+        zapdos = {
+          host = "zapdos";
+          forwardAgent = true;
+        };
       };
-      zapdos = {
-        host = "zapdos";
-        forwardAgent = true;
+    };
+    systemd.user.services.afuse-sshfs = {
+      Unit = {
+        Description = "afuse sshfs automount";
       };
+      Service = {
+        Type = "simple";
+        RuntimeDirectory = "sshfs";
+        RuntimeDirectoryPreserve = true;
+        ExecStart = ''
+          ${pkgs.afuse}/bin/afuse -f \
+            -o timeout=60 \
+            -o auto_unmount \
+            -o flushwrites \
+            -o mount_template="${pkgs.sshfs}/bin/sshfs -o reconnect %%r: %%m" \
+            -o unmount_template="umount -l %%m" \
+            %t/sshfs
+        '';
+      };
+      Install.WantedBy = ["default.target"];
     };
   };
 }
