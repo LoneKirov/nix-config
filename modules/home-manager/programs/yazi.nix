cd1161506d3d70eea13a6e40dc5b27f2e00658ad
Adam Miller <github@adammill.dev>
2026-01-23 23:42:00 -0800

    yazi updates
    - enable rar support
    - add toggle-pane, recycle-bin, and starship plugins
    - make sure various cli utilities yazi can use are included as extra
      packages
    - improve image preview quality and resolution

--- modules/home-manager/programs/yazi.nix
+++ modules/home-manager/programs/yazi.nix
@@ -1,3 +1,49 @@
-{...}: {
-  programs.yazi.enable = true;
+{pkgs, ...}: {
+  programs.yazi = {
+    enable = true;
+    package = pkgs.yazi.override {_7zz = pkgs._7zz-rar;};
+    plugins = {
+      inherit (pkgs.yaziPlugins) toggle-pane recycle-bin starship;
+    };
+    extraPackages = with pkgs; [
+      ffmpeg
+      jq
+      poppler
+      fd
+      ripgrep
+      fzf
+      zoxide
+      resvg
+      wl-clipboard-rs
+      imagemagick
+      trash-cli
+      starship
+    ];
+    initLua = ''
+      require("recycle-bin"):setup()
+      require("starship"):setup()
+    '';
+    settings = {
+      preview = {
+        image_filter = "lanczos3";
+        image_quality = 90;
+        max_width = 4000;
+        max_height = 4000;
+      };
+    };
+    keymap = {
+      mgr.prepend_keymap = [
+        {
+          on = "T";
+          run = "plugin toggle-pane max-preview";
+          desc = "Maximize or restore the preview pane";
+        }
+        {
+          on = ["R" "b"];
+          run = "plugin recycle-bin";
+          desc = "Open Recycle Bin menu";
+        }
+      ];
+    };
+  };
 }
