5f069cfac49cea99c2464e6a6046ac9980e10f76
Adam Miller <github@adammill.dev>
2026-01-25 18:45:58 -0800

    remove framework-amd-ai-300-series override to disable ucm. a better workaround has been pushed into nixos-hardware

--- hosts/mew/nixos/framework-amd-ai-300-series/audio.nix
+++
@@ -1,8 +0,0 @@
-{...}: {
-  # https://github.com/NixOS/nixos-hardware/issues/1603
-  services.pipewire.wireplumber.extraConfig.no-ucm = {
-    "monitor.alsa.properties" = {
-      "alsa.use-ucm" = false;
-    };
-  };
-}
--- hosts/mew/nixos/framework-amd-ai-300-series/default.nix
+++ hosts/mew/nixos/framework-amd-ai-300-series/default.nix
@@ -1,7 +1,6 @@
 {inputs, ...}: {
   imports = [
     inputs.nixos-hardware.nixosModules.framework-amd-ai-300-series
-    ./audio.nix
     ./bluetooth.nix
     ./fingerprint.nix
     ./hibernate
