653ed3b26dce2f050397f7f7a3efecbc96029642
Adam Miller <github@adammill.dev>
2026-01-26 20:37:36 -0800

    use ExecCondition instead of ExecStartPre

--- hosts/mew/nixos/framework-amd-ai-300-series/fingerprint.nix
+++ hosts/mew/nixos/framework-amd-ai-300-series/fingerprint.nix
@@ -4,7 +4,7 @@
   ...
 }: {
   # https://community.frame.work/t/only-enable-fingerprint-authentication-when-lid-is-open/21544/8
-  systemd.services.fprintd.preStart = ''
+  systemd.services.fprintd.serviceConfig.ExecCondition = ''
     ${lib.getExe' pkgs.gnugrep "grep"} -q open /proc/acpi/button/lid/LID0/state
   '';
 }
