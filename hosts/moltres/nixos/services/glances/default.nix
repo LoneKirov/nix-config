f5868088de5a0a75d789081a6d71d6cb7d2bcf2f
Adam Miller <github@adammill.dev>
2026-02-28 17:58:30 -0800

    moltres: enable container monitoring through docker-socket-proxy

--- hosts/moltres/nixos/services/glances/default.nix
+++ hosts/moltres/nixos/services/glances/default.nix
@@ -1,4 +1,8 @@
-{...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   services.glances = {
     enable = true;
     extraArgs = [
@@ -6,5 +10,51 @@
       "${./glances.conf}"
       "-w"
     ];
+    # nixpkgs doesn't include optional deps for container monitoring
+    package = pkgs.glances.overrideAttrs (old: {
+      propagatedBuildInputs =
+        (old.propagatedBuildInputs or [])
+        ++ [
+          pkgs.python3Packages.docker
+          pkgs.python3Packages.requests
+          pkgs.python3Packages.python-dateutil
+        ];
+    });
+  };
+
+  systemd.services.glances = {
+    environment = {
+      DOCKER_HOST = "127.0.0.1:2375";
+    };
+    requires = ["docker-socket-proxy.service"];
+  };
+
+  virtualisation.quadlet.containers.docker-socket-proxy = let
+    host-uid = "0";
+    host-gid = "998"; # podman
+    container-uid = "0";
+    container-gid = "0";
+  in {
+    unitConfig = {
+      Description = "Proxy podman socket as read-only docker tcp endpoint";
+      Requires = [config.systemd.sockets.podman.name];
+    };
+    containerConfig = {
+      image = "docker.io/tecnativa/docker-socket-proxy";
+      autoUpdate = "registry";
+      userns = "auto";
+      environments = {
+        CONTAINERS = "1";
+        IMAGES = "1";
+      };
+      publishPorts = ["127.0.0.1:2375:2375"];
+      volumes = [
+        "/var/run/podman/podman.sock:/var/run/docker.sock:ro,idmap=uids=@${host-uid}-${container-uid}-1;gids=@${host-gid}-${container-gid}-1"
+      ];
+    };
+    serviceConfig = {
+      Restart = "on-failure";
+    };
+    autoStart = true;
   };
 }
