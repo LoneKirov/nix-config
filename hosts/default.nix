d6dff389ddedff66b4e348b4fa0f0ae37e77cb20
Adam Miller <github@adammill.dev>
2026-03-06 11:01:45 -0800

    disable gateway for now

--- hosts/default.nix
+++ hosts/default.nix
@@ -1,6 +1,6 @@
 {
   internal-lib,
-  withSystem,
+  # withSystem,
   ...
 }: {
   config.flake = {
@@ -8,8 +8,8 @@
       mew = import ./mew {inherit internal-lib;};
       moltres = import ./moltres {inherit internal-lib;};
     };
-    homeConfigurations = {
-      "kirov@gateway" = import ./gateway {inherit internal-lib withSystem;};
-    };
+    # homeConfigurations = {
+    #   "kirov@gateway" = import ./gateway {inherit internal-lib withSystem;};
+    # };
   };
 }
