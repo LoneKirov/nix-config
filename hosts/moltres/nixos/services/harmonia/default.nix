2b2f31c54c163fa571929e66c9fd5877367357fd
Adam Miller <github@adammill.dev>
2026-03-25 19:29:26 -0700

    moltres: switch to default nix gc service
    use default nix gc service to allow longer retention for harmonia

--- hosts/moltres/nixos/services/harmonia/default.nix
+++ hosts/moltres/nixos/services/harmonia/default.nix
@@ -9,4 +9,18 @@
 
     signKeyPaths = [config.sops.secrets.harmonia.path];
   };
+
+  # disable detnix automatic gc to make better use of store as cache
+  environment.etc."determinate/config.json".text = ''
+    {
+      "garbageCollector": {
+        "strategy": "disabled"
+      }
+    }
+  '';
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
 }
