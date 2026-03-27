0cf7e02288f786c0902d67d75f4c69ff8faa935f
Adam Miller <github@adammill.dev>
2026-03-27 01:03:16 -0700

    set tailscale network as local for syncthing

--- hosts/moltres/nixos/services/syncthing.nix
+++ hosts/moltres/nixos/services/syncthing.nix
@@ -6,5 +6,9 @@
     dataDir = "/srv/syncthing";
     overrideDevices = false;
     overrideFolders = false;
+    settings.options.alwaysLocalNets = [
+      "100.64.0.0/10"
+      "fd7a:115c:a1e0::/48"
+    ];
   };
 }
--- modules/home-manager/programs/syncthing.nix
+++ modules/home-manager/programs/syncthing.nix
@@ -7,5 +7,9 @@
     enable = lib.mkDefault config.local.programs.dms-shell.enable;
     overrideDevices = false;
     overrideFolders = false;
+    settings.options.alwaysLocalNets = [
+      "100.64.0.0/10"
+      "fd7a:115c:a1e0::/48"
+    ];
   };
 }
