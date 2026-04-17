d4f0a12b5311700ec62e5f5977c739ba1f1b885b
Adam Miller <github@adammill.dev>
2026-04-17 00:53:18 -0700

    articuno: re-enable PCRs 0-3

--- hosts/articuno/nixos/lanzaboote.nix
+++ hosts/articuno/nixos/lanzaboote.nix
@@ -1,17 +1,7 @@
 {lib, ...}: {
-  boot.lanzaboote.measuredBoot = {
-    # https://github.com/nix-community/lanzaboote/pull/564#issuecomment-41896858291
-    upstreamStaticMeasurements = lib.mkForce [
-      "500-separator.pcrlock.d/300-0x00000000.pcrlock"
-      "400-secureboot-separator.pcrlock.d/300-0x00000000.pcrlock"
-    ];
-    pcrs = lib.mkForce [
-      # 0 # platform-code
-      # 1 # platform-config
-      # 2 # external-code
-      # 3 # external-config
-      4 # boot-loader-code
-      7 # secure-boot-policy
-    ];
-  };
+  # https://github.com/nix-community/lanzaboote/pull/564#issuecomment-41896858291
+  boot.lanzaboote.measuredBoot.upstreamStaticMeasurements = lib.mkForce [
+    "500-separator.pcrlock.d/300-0x00000000.pcrlock"
+    "400-secureboot-separator.pcrlock.d/300-0x00000000.pcrlock"
+  ];
 }
