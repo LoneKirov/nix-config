0537eba417cb31f8d53a69d8a8fdae6c824832d0
Adam Miller <github@adammill.dev>
2026-04-18 00:42:13 -0700

    starship: show hostname on headless systems

--- modules/home-manager/programs/starship/default.nix
+++ modules/home-manager/programs/starship/default.nix
@@ -7,11 +7,13 @@
     {enable = lib.mkDefault true;}
     (lib.mkIf config.programs.starship.enable {
       settings =
+        lib.recursiveUpdate
         {
           "$schema" = "https://starship.rs/config-schema.json";
           direnv.disabled = false;
+          hostname.ssh_only = config.local.services.xserver.enable;
         }
-        // fromTOML (builtins.readFile ./nerd-font-symbols.toml);
+        (fromTOML (builtins.readFile ./nerd-font-symbols.toml));
     })
   ];
 }
