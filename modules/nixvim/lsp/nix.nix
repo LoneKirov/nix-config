0de434a23c5484eac071fe2496ef2ff040abdd0e
Adam Miller <github@adammill.dev>
2026-06-17 01:23:59 -0700

    nix lsp updates
    - fix nixd configuration
    - add nixos/home-manager exprs
    - remove nil

--- modules/nixvim/lsp/nix.nix
+++ modules/nixvim/lsp/nix.nix
@@ -5,16 +5,29 @@
 }: {
   lsp = {
     servers = {
-      nil_ls = {
-        enable = true;
-        config.settings.nil = {
-          formatting.command = ["${lib.getExe pkgs.alejandra}"];
-        };
-      };
       nixd = {
         enable = true;
-        config.settings = {
+        config.settings.nixd = let
+          flake = ''(builtins.getFlake (builtins.toString ./.))'';
+        in {
+          nixpkgs.expr = "import ${flake}.inputs.nixpkgs {}";
+
           formatting.command = ["${lib.getExe pkgs.alejandra}"];
+
+          options = {
+            nixos.expr = ''
+              let
+                hostname = builtins.replaceStrings ["\n"] [""] (builtins.readFile "/etc/hostname");
+              in
+                ${flake}.nixosConfigurations.''${hostname}.options
+            '';
+            home-manager.expr = ''
+              let
+                hostname = builtins.replaceStrings ["\n"] [""] (builtins.readFile "/etc/hostname");
+              in
+                ${flake}.nixosConfigurations.''${hostname}.options.home-manager.users.type.getSubOptions []
+            '';
+          };
         };
       };
     };
