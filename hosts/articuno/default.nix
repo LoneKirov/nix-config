b866c7bcb7c77745793407be94b704254a55112b
Adam Miller <github@adammill.dev>
2026-07-09 23:16:40 -0700

    articuno: Add MT6639 (MT7927) Bluetooth firmware

--- hosts/articuno/default.nix
+++ hosts/articuno/default.nix
@@ -15,6 +15,19 @@
           ./lanzaboote.nix
           ./nvidia.nix
           ./services
+          # mediatek: Add MT6639 (MT7927) Bluetooth firmware
+          ({pkgs, ...}: {
+            hardware.firmware = [
+              (pkgs.runCommand "MT7927" {} ''
+                mkdir -p $out/lib/firmware/mediatek
+
+                cp ${pkgs.fetchurl {
+                  url = "https://gitlab.com/kernel-firmware/linux-firmware/-/raw/77ad2a92acf2ac3e5ea47432b43d925ff99db909/mediatek/mt7927/BT_RAM_CODE_MT6639_2_1_hdr.bin";
+                  hash = "sha256-ZpxcmaDFnIXBKF09G4sxkVwtMTQaIkT07dy/1g/7vHY=";
+                }} $out/lib/firmware/mediatek/BT_RAM_CODE_MT6639_2_1_hdr.bin
+              '')
+            ];
+          })
         ];
 
         networking.hostName = "articuno";
