7a63eee2b755623891fb120d4dadc080ff154f08
Adam Miller <github@adammill.dev>
2026-08-19 23:37:25 -0700

    articuno-wsl: cleanup orphaned winsocat instances

--- hosts/articuno-wsl/wsl/rbw.nix
+++ hosts/articuno-wsl/wsl/rbw.nix
@@ -31,17 +31,26 @@
 
           Service = let
             inherit (config.services.rbw-agent) windowsSocket;
+            cleanupScript = pkgs.writeShellScript "stop-rbw-agent-bridge" ''
+              export WSL_INTEROP=$(${lib.getExe' pkgs.iproute2 "ss"} -xl | grep -o '/run/WSL/[0-9]*_interop' | head -n 1)
+              /mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -NoProfile -Command \
+                "Get-CimInstance Win32_Process -Filter 'Name = \"winsocat.exe\"' | Where-Object { \$_.CommandLine -match 'rbw-agent.sock' } | ForEach-Object { Stop-Process -Id \$_.ProcessId -Force }" \
+                2>/dev/null || true
+              ${lib.getExe' pkgs.coreutils "rm"} -f ${windowsSocket.wslPath}
+            '';
           in {
             Type = "simple";
             Environment = [''SSH_AUTH_SOCK="%t/rbw/ssh-agent-socket"''];
-            ExecStartPre = "${lib.getExe' pkgs.coreutils "rm"} -f ${windowsSocket.wslPath}";
-            ExecStart = "${pkgs.writeShellScript "rbw-agent-socket-bridge" ''
+            ExecStartPre = "${cleanupScript}";
+            ExecStart = "${pkgs.writeShellScript "start-rbw-agent-bridge" ''
               export WSL_INTEROP=$(${lib.getExe' pkgs.iproute2 "ss"} -xl | grep -o '/run/WSL/[0-9]*_interop' | head -n 1)
+
               WIN_WINSOCAT_PATH=$(/mnt/c/Windows/System32/where.exe winsocat.exe 2>/dev/null | head -n 1 | tr -d '\r')
               WSL_WINSOCAT_PATH=$(/sbin/wslpath -u "$WIN_WINSOCAT_PATH")
 
               exec $WSL_WINSOCAT_PATH "UNIX-LISTEN:${windowsSocket.winPath}" WSL:"${lib.getExe pkgs.socat} STDIO unix-connect:$SSH_AUTH_SOCK"
             ''}";
+            ExecStop = "${cleanupScript}";
             Restart = "on-failure";
           };
 
