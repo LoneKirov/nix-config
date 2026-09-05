b27d42da5a691a800c4dee99d09a43da77ec398d
Adam Miller <github@adammill.dev>
2026-09-04 22:05:48 -0700

    enable jujutsu watchman backend

--- modules/home-manager/programs/jujutsu.nix
+++ modules/home-manager/programs/jujutsu.nix
@@ -1,16 +1,31 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }: {
   config = let
     inherit (config.programs.jujutsu) enable;
+    jj-with-watchman = pkgs.symlinkJoin {
+      name = "jujutsu-with-watchman";
+      paths = [pkgs.jujutsu];
+      nativeBuildInputs = [pkgs.makeWrapper];
+      postBuild = ''
+        wrapProgram $out/bin/jj \
+          --prefix PATH : ${lib.makeBinPath [pkgs.watchman]}
+      '';
+    };
   in {
     programs = {
       jujutsu = {
         enable = lib.mkDefault config.programs.git.enable;
+        package = jj-with-watchman;
         settings = {
           user = config.programs.git.settings.user;
+          fsmonitor = {
+            backend = "watchman";
+            watchman.register-snapshot-trigger = true;
+          };
         };
       };
       jjui.enable = enable;
