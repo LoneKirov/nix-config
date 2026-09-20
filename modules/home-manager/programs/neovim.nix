108f5bc5386cfb1abc386b8a2e319c0b5ecd5810
Adam Miller <github@adammill.dev>
2026-09-20 06:19:04 -0700

    use nvim as manpager

--- modules/home-manager/programs/neovim.nix
+++ modules/home-manager/programs/neovim.nix
@@ -1,14 +1,22 @@
 {
+  config,
   inputs,
   lib,
   ...
 }: {
   imports = [inputs.nixvim.homeModules.nixvim];
 
-  config.programs.nixvim = {
-    enable = lib.mkDefault true;
-    defaultEditor = true;
-    imports = [../../nixvim];
-    vimdiffAlias = true;
+  config = let
+    inherit (config.programs.nixvim) enable build;
+  in {
+    programs.nixvim = {
+      enable = lib.mkDefault true;
+      defaultEditor = true;
+      imports = [../../nixvim];
+      vimdiffAlias = true;
+    };
+    home.sessionVariables = lib.mkIf enable {
+      MANPAGER = "${lib.getExe build.package} -c 'Man!'";
+    };
   };
 }
